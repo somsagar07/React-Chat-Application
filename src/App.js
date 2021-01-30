ec3521597c2debbdd793b4f717d3e420b1d5355f
Som Sagar <58594538+somsagar07@users.noreply.github.com>
2021-01-30 21:53:59 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { ChatEngine } from 'react-chat-engine';
+import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+            height="100vh"
+            projectID="71b09097-0b60-4d3e-965e-453e34fbf4bd"
+            userName="revolutionaryagent_"
+            userSecret="agent@123"
+        />
+    );
+}
+export default App;
